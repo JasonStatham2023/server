eebfda7dd5c12b19d75653c16c006f39e0a4bc19
Jason <229555644@qq.co>
2022-12-06 22:55:02 +0800

    代码提交

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,23 +2,28 @@ import { Injectable } from '@nestjs/common';
 
 // This should be a real class/interface representing a user entity
 export type User = any;
+const users: User[] = [
+  {
+    userId: 1,
+    username: 'john',
+    password: 'changeme',
+  },
+  {
+    userId: 2,
+    username: 'maria',
+    password: 'guess',
+  },
+];
 
 @Injectable()
 export class UsersService {
-  private readonly users = [
-    {
-      userId: 1,
-      username: 'john',
-      password: 'changeme',
-    },
-    {
-      userId: 2,
-      username: 'maria',
-      password: 'guess',
-    },
-  ];
+  constructor(readonly users) {
+    this.users = users;
+  }
 
   async findOne(username: string): Promise<User | undefined> {
-    return this.users.find((user) => user.username === username);
+    return Promise.resolve(
+      this.users.find((user) => user.username === username),
+    );
   }
 }
